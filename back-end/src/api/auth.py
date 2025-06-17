adb97decf15a53f1c2dc835072291453590f47d9
Sboof <amaach@student.1337.ma>
2025-06-17 17:13:41 +0100

    Implement user registration with password hashing and add user profile fields

--- back-end/src/api/auth.py
+++ back-end/src/api/auth.py
@@ -1,22 +1,36 @@
 from fastapi import APIRouter
-from pydantic import BaseModel, Field, EmailStr
+from pydantic import BaseModel, Field, EmailStr, field_validator
+from db import db
+from core.security import hash_password, verify_password
+from typing import Literal
+import random
 
 router = APIRouter()
 
-class RegisterRequest(BaseModel):
+class UserRegister(BaseModel):
     name: str = Field(..., min_length=2, max_length=50)
     username: str = Field(..., min_length=3, max_length=20)
     email: EmailStr
     password: str = Field(..., min_length=6, max_length=100)
+    @field_validator("password", mode='after')
+    def preprocess_word(cls, v):
+        return hash_password(v)
+    profilePicture: str = Field(default=f'https://i.pravatar.cc/150?img={random.randint(1, 70)}')
+    watchedMovies: list[str] = Field(default_factory=list)
+    languagePreference: str = Field(default='en')
+    role: Literal['user', 'admin'] = Field(default='user')
 
+class User(BaseModel):
+    id: str
+    name: str
+    username: str
+    email: EmailStr
+    profilePicture: str
+    watchedMovies: list[str]
+    languagePreference: str
 
 @router.post("/register")
-def register(data: RegisterRequest):
-    print(f"Registering user: {data.name}, {data.username}, {data.email}")
-    return {
-        "id": 1,
-        "name": data.name,
-        "username": data.username,
-        "email": data.email,
-        "password": data.password
-    }
+async def register(user: UserRegister):
+    result = await db.users.insert_one(user.model_dump())
+
+    return User(**user.model_dump(), id=str(result.inserted_id))
